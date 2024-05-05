3e31186329c95d49f54f897b2d3e0ba37e35f0fb
paul-el-kouba <paul.elkouba@lau.edu>
2024-05-06 00:32:32 +0300

    [FIX] fix dictionary key,value

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ from sentiment_learner import Sentiment_Learner
 def empty_directory():
     directory = "./midi_files/midi/"
     directory2 = "./midi_files/features/"
-    shutil.rmtree(directory)
-    shutil.rmtree(directory2)
+    try:
+        shutil.rmtree(directory)
+        shutil.rmtree(directory2)
+    except:
+        pass
 
     os.makedirs(directory)
     os.makedirs(directory2)
@@ -52,7 +55,8 @@ file_emotion_dict = {}
 for file in os.listdir(midi_dir):
     if file.endswith(".mid"):
         midi_file = os.path.join(midi_dir, file)
-        theme, predicted = sentimentLearner.predict_sentiments(midi_file)
+        temp_dict = sentimentLearner.predict_sentiments(midi_file)
+        theme, predicted = temp_dict.items()
         file_emotion_dict[theme] = predicted
 
 distances = {file: similarityMeasure.euclidean(target_emotions, predicted_emotions) for file, predicted_emotions in file_emotion_dict.items()}
